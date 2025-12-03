25ebde345a9b5b5a179dd29f3035dcc1d884711f
dedsec <dedsec@leviathan.0xdedsec>
2025-12-03 14:15:50 +0530

    refactor: Remove specific themes and battery rendering functionality.

--- firmware/src/themes/Theme.h
+++ firmware/src/themes/Theme.h
@@ -9,6 +9,5 @@ public:
   virtual void renderTime(Adafruit_SSD1306& disp, String time, String date, String day) = 0;
   virtual void renderWeather(Adafruit_SSD1306& disp, const WeatherData& data) = 0;
   virtual void renderMusic(Adafruit_SSD1306& disp, String song, String artist, bool isPlaying) = 0;
-  virtual void renderBattery(Adafruit_SSD1306& disp, uint8_t percentage, bool isLow) = 0;
 };
 
